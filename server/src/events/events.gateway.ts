f5e8ff6e1d155945cfc4c84a47ca782fffc320a9
Kim Gyumin <diewhite@icloud.com>
2023-04-28 20:57:17 +0900

    웹소켓 접속 시 이름 지정 map 형태로 name,id 저장 테스트

--- server/src/events/events.gateway.ts
+++ server/src/events/events.gateway.ts
@@ -1,5 +1,7 @@
 import {
     MessageBody,
+    OnGatewayConnection,
+    OnGatewayDisconnect,
     SubscribeMessage,
     WebSocketGateway,
     WebSocketServer,
@@ -7,17 +9,27 @@ import {
   } from '@nestjs/websockets';
   import { from, Observable } from 'rxjs';
   import { map } from 'rxjs/operators';
-  import { Server } from 'socket.io';
+  import { Socket, Server } from 'socket.io';
   
   @WebSocketGateway(4005, {
     cors: {
       origin: '*',
     },
   })
-  export class EventsGateway {
+  
+  export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect{
+    private users = new Map();
+
+    handleConnection(client: Socket) {
+      console.log(Socket.name+" connected!");
+      
+    }
+    handleDisconnect(client: Socket) {
+      console.log(Socket.name+" disconnected!");
+    }
     @WebSocketServer()
     server: Server;
-  
+ 
     @SubscribeMessage('events')
     findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
       return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
@@ -26,5 +38,18 @@ import {
     @SubscribeMessage('identity')
     async identity(@MessageBody() data: number): Promise<number> {
       return data;
+    }
+
+    @SubscribeMessage('chat')
+    async onChat(@MessageBody() data: string): Promise<string> {
+      return data;
+    }
+
+    @SubscribeMessage('information')
+    information(@MessageBody() information: any){
+      console.log("id: "+information.id);
+      console.log("name: "+information.name);
+      this.users.set(information.name,information.id);
+      console.log("map에서 test호출"+this.users.get("test"));
     }
   }
